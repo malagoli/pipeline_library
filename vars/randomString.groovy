c0635e658ffab10f330486410ceb1c6a56b99619
Davide Malagoli <malagoli@gmail.com>
2018-12-29 17:19:15 +0100

    fixed variable generator

--- vars/randomString.groovy
+++ vars/randomString.groovy
@@ -1,12 +1,14 @@
 package li.malago
 
-def generator = { String alphabet, int n ->
-  new Random().with {
-    (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
-  }
-}
 
 
 def call(int size){
-    return generator( (('A'..'Z')+('0'..'9')).join(), size )
+  def generator = { String alphabet, int n ->
+    new Random().with {
+      (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
+    }
+  }
+
+
+  return generator( (('A'..'Z')+('0'..'9')).join(), size )
 }
