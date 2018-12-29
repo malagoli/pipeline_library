ceb13789a77fd7c561e49ce547a23d659790b97a
Davide Malagoli <malagoli@gmail.com>
2018-12-29 17:24:36 +0100

    minimal version

--- vars/randomString.groovy
+++ vars/randomString.groovy
@@ -1,14 +1,5 @@
 package li.malago
 
-
-
 def call(int size){
-  def generator = { String alphabet, int n ->
-    new Random().with {
-      (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
-    }
-  }
-
-
-  return generator( (('A'..'Z')+('0'..'9')).join(), size )
+  return new Random().with {(1..size).collect {(('a'..'z')).join()[ nextInt((('a'..'z')).join().length())]}.join()}
 }
